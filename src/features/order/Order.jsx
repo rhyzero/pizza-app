b27709f6f41db57d79c619c26d5e39d7e0168652
Jacky Chen <jackyc4200@gmail.com>
2024-01-28 00:57:05 -0500

    added cart items to order page

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -2,6 +2,7 @@
 
 import { useLoaderData } from 'react-router-dom';
 import { getOrder } from '../../services/apiRestaurant';
+import OrderItem from './OrderItem';
 import {
   calcMinutesLeft,
   formatCurrency,
@@ -50,7 +51,11 @@ function Order() {
           (Estimated delivery: {formatDate(estimatedDelivery)})
         </p>
       </div>
-
+      <ul className="divide-y divide-stone-200 border-b border-t">
+        {cart.map((item) => (
+          <OrderItem item={item} key={item.id} />
+        ))}
+      </ul>
       <div className="space-y-2 bg-stone-200 px-6 py-5">
         <p className="text-sm font-medium text-stone-600">
           Price pizza: {formatCurrency(orderPrice)}
